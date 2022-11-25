e3d3d1f79456a4b7354a4e60c1decec6665cc836
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-11-25 09:56:31 +0300

    Initial Commit

---
+++ Tour 34 MyScheduleApp/Tour_34_MyScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tour_34_MyScheduleAppApp.swift
+//  Tour 34 MyScheduleApp
+//
+//  Created by Collins Muthomi on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tour_34_MyScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
